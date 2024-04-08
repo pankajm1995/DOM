d335ba3189e89d9f51210b6d499986f1052c7d41
Pankaj Kumar Maurya <153347011+pankajm1995@users.noreply.github.com>
2024-04-08 22:02:03 +0000

    update

---
+++ project/index.js
@@ -0,0 +1,41 @@
+let form=document.getElementById("signup")
+
+form.addEventListener("submit",(e)=>
+{
+    e.preventDefault();
+
+    let name1=document.getElementById("name").value
+    let email1=document.getElementById("email").value
+    let con=document.getElementById("country").value
+    let mob =document.getElementById("phone").value
+    let pass= document.getElementById("password").value
+
+    let user={
+        name:name1,
+        email:email1,
+        country:con,
+        phone:mob,
+        password:pass
+    }
+
+    console.log(user);
+})
+
+let form1 = document.getElementById("signin")
+
+form1.addEventListener("submit",(e)=>
+{
+    e.preventDefault();
+
+    let name2=document.getElementById("signin-name").value
+    let email2= document.getElementById("signin-email").value
+    let pass2=document.getElementById("signin-password").value
+
+    let data={
+        name:name2,
+        email:email2,
+        password:pass2
+    }
+
+    console.log(data);
+})
